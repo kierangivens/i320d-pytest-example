a8361a70f8aa0f0e3d682b0560871b10ec6d5003
CrispEggo <kierangivens@yahoo.com>
2025-03-26 16:44:58 -0500

    Test now documents why tests are failing

--- my_code.py
+++ my_code.py
@@ -5,9 +5,9 @@ def fix_phone_num(phone_num_to_fix):
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
   four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
-  
-  fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
-  
+
+  fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part
+
   return fixed_num
 
 def test_fix_phone_num():
@@ -20,3 +20,7 @@ def test_fix_phone_handles_dashes():
 
 def test_fix_phone_handles_parentheses():
   assert fix_phone_num("(321) 654 3333") == '(321) 654 3333'
+
+def test_all_digits():
+  with pytest.raises(ValueError):
+    fix_phone_num("512555OO23")
